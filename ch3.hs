740c48a2a0ae11b4447cae43de9bdda40da3e171
Chanon <chanon.chanpiwat@gmail.com>
2023-02-05 21:53:59 +0700

    ch3

---
+++ ch3.hs
@@ -0,0 +1,16 @@
+-- 1.
+-- ['a','b','c'] :: [char,char,char]
+-- ('a','b','c') :: (char,char,char)
+-- [(Fals,'O'),(True,'1')] :; [(bool,char)]
+-- ([False, True],['0','1']) :: ([bool],[char])
+-- [tail,init,reverse] :: [[a] -> [a]]
+--
+--
+--2. second xs = head(tail xs) :: [a] -> a
+--   swap (x,y) = (y,x) :: (a,b) -> (b,a)
+--   pair x y = (x,y) :: a -> b -> (a,b)
+--   double x = x * 2 :: Num a => a -> a
+--   palindrome xs = reverse xs == xs :: Eq a => [a] -> bool
+--   twice f x = f (f x) :: (t -> t) -> t -> t
+--
+--   3. checked
